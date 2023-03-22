5d2de831da125d268d460a144b260a943f472437
elyo9381 <won4413@daum.net>
2023-03-22 23:00:00 +0900

    feature calculator & calculatorTest

---
+++ src/test/java/com/example/union/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.example.union;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void add(){
+        Calculator cal = new Calculator();
+    }
+}
