49aa94e66356e584bdf3f0e3b7005a381619a924
elyo9381 <won4413@daum.net>
2023-03-22 23:05:02 +0900

    feature calculator add & subtract

--- src/test/java/com/example/union/CalculatorTest.java
+++ src/test/java/com/example/union/CalculatorTest.java
@@ -1,11 +1,23 @@
 package com.example.union;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
     @Test
     public void add(){
         Calculator cal = new Calculator();
+        cal.add(1, 2);
+
+        assertThat(cal.add(1,2)).isEqualTo(3);
+    }
+
+    @Test
+    public void subtract(){
+        Calculator calculator = new Calculator();
+        assertThat(calculator.subtract(2,1)).isEqualTo(1);
     }
 }
